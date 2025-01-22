2e683a86fccccec24210bfe517bffac11e0236b2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-22 22:50:00 +0700

    feat : index component.

---
+++ src/app/components/Utilities/index.ts
@@ -0,0 +1,6 @@
+import Input from "./Input";
+import TableComponent from "./Table";
+import ButtonComponent from "./Button";
+import AuthorComponent from "./Author";
+
+export { Input, TableComponent, ButtonComponent, AuthorComponent }
